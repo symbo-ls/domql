b05da30afa2fbce18494944dc44f7f1a0d8f060b
nikoloza <nika.tomadze@gmail.com>
2016-05-12 19:47:40 +0200

    running yay

--- example/index.js
+++ example/index.js
@@ -2,15 +2,24 @@
 
 var $ = require('../lib')
 
-var afrika = $.Element.create({
-  'nodeName': 'div',
-  'text': 'Hello Afrika',
-  'attr': {
-    'align': 'right'
-  },
-  'styles': {
-    'background': 'black',
-    'color': 'white',
-    'padding': '10px'
-  }
-})
+var now = performance.now()
+
+for (let i = 0; i < 14000; i++) {
+  var afrika = $.Element.create({
+    'nodeName': 'div',
+    'text': 'Hello Afrika',
+    'attr': {
+      'align': 'right'
+    },
+    'styles': {
+      'background': 'black',
+      'color': 'white',
+      'padding': '10px'
+    }
+  })
+}
+
+var later = performance.now()
+
+console.log(later - now)
+console.log($.Element.tree)
