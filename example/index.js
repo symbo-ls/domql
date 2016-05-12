775df18813cb58ea729db81944159e68bd01fad2
nikoloza <nika.tomadze@gmail.com>
2016-05-12 18:10:55 +0200

    added example

---
+++ example/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var $ = require('../lib')
+
+var afrika = $.Element.create({
+  'nodeName': 'div',
+  'text': 'Hello Afrika',
+  'attr': {
+    'align': 'right'
+  },
+  'styles': {
+    'background': 'black',
+    'color': 'white',
+    'padding': '10px'
+  }
+})
