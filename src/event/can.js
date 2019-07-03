d03221ba29922e8164932a1bd8aabc7778875fb6
nikoloza <nika.tomadze@gmail.com>
2019-07-03 10:34:02 +0400

    added size-limit

--- src/event/can.js
+++ src/event/can.js
@@ -1,13 +1,14 @@
 'use strict'
 
-import _ from 'lodash'
 import Elem from '../element'
 import Err from '../res/error'
+
+import indexOf from 'lodash.indexof'
 
 export default {
   render (element) {
     var tag = element.tag || 'div'
-    var isValid = _.indexOf(Elem.nodes.names, tag) > -1
+    var isValid = indexOf(Elem.nodes.names, tag) > -1
     return isValid || Err('HTMLInvalidTag')
   }
 }
