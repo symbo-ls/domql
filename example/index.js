2017feabdf3b200289ff0a6578608a594ef92b89
nikoloza <nika.tomadze@gmail.com>
2019-07-03 09:37:31 +0400

    cleaned up

--- example/index.js
+++ example/index.js
@@ -1,6 +1,5 @@
 'use strict'
 
-<<<<<<< HEAD
 import DOM from '../src'
 
 var { performance } = window
@@ -8,14 +7,6 @@ var now = performance.now()
 
 for (let i = 0; i < 1000; i++) {
   var afrika = DOM.create({
-=======
-var $ = require('../lib')
-
-var now = performance.now()
-
-for (let i = 0; i < 1000; i++) {
-  var afrika = $.Element.create({
->>>>>>> 966f98521373dfa2901abdda7210eaa80a46ac31
     text: 'Hello Afrika - ' + parseInt(Math.random() * 100),
     attr: {
       align: 'right'
@@ -27,11 +18,7 @@ for (let i = 0; i < 1000; i++) {
     }
   })
 
-<<<<<<< HEAD
   DOM.create({
-=======
-  var lion = $.Element.create({
->>>>>>> 966f98521373dfa2901abdda7210eaa80a46ac31
     text: 'Lion',
     tag: 'span',
     style: {
@@ -41,11 +28,7 @@ for (let i = 0; i < 1000; i++) {
     }
   }, afrika)
 
-<<<<<<< HEAD
   DOM.create('yay', afrika)
-=======
-  var yay = $.Element.create('yay', afrika)
->>>>>>> 966f98521373dfa2901abdda7210eaa80a46ac31
 }
 
 var later = performance.now()
