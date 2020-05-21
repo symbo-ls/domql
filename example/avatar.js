c147207885070fd286dd8bbe9e3c5deb1d702a07
nikoloza <nika.tomadze@gmail.com>
2020-05-21 22:28:12 +0400

    changed example name

---
+++ example/avatar.js
@@ -0,0 +1,33 @@
+'use strict'
+
+export default {
+  query: true,
+  shape: 'ShapeA',
+  scheme: 'GroupA',
+  style: {
+    paddingLeft: 'E1',
+    paddingRight: 'E1'
+  },
+  avatar: {
+    query: true
+  },
+  title: {
+    query: true,
+    style: {
+      flex: 1
+    },
+    on: {
+      data: {},
+      hover: (e) => {
+        console.log(e)
+      }
+    }
+  },
+  close: {
+    type: 'icon',
+    style: {
+      paddingLeft: 'B4',
+      paddingRight: 'B4'
+    }
+  }
+}
