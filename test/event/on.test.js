d2cabb8583fd66b784b3ebc71f06eae328cad115
nikoloza <nika.tomadze@gmail.com>
2020-07-12 06:53:46 +0400

    added event tests

--- test/event/on.test.js
+++ test/event/on.test.js
@@ -3,8 +3,34 @@
 import 'regenerator-runtime/runtime'
 import create from '../../src/element/create'
 
-var dom = create({})
-
-test('should clone the object', () => {
-  expect(dom).toStrictEqual(dom)
+test('check if all element events are firing', () => {
+  create({
+    on: {
+      init: (element) => {
+      },
+      render: (element, state) => {
+      },
+      click: (event, element) => {
+        expect(event).toBeInstanceOf(window.Event)
+      },
+      change: (event, element) => {
+        expect(event).toBeInstanceOf(window.Event)
+      },
+      down: (event, element) => {
+        expect(event).toBeInstanceOf(window.Event)
+      },
+      move: (event, element) => {
+        expect(event).toBeInstanceOf(window.Event)
+      },
+      up: (event, element) => {
+        expect(event).toBeInstanceOf(window.Event)
+      },
+      load: (event, element) => {
+        expect(event).toBeInstanceOf(window.Event)
+      },
+      input: (event, element) => {
+        expect(event).toBeInstanceOf(window.Event)
+      }
+    }
+  })
 })
