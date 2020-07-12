7031d2c5f0b4bb6ad97dc8bb6e6ef1ddd0d0baaf
nikoloza <nika.tomadze@gmail.com>
2020-07-12 06:55:07 +0400

    can tests

---
+++ test/event/can.test.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import 'regenerator-runtime/runtime'
+import { can } from '../../src/event'
+
+test('checks with valid tags', () => {
+  expect(can.render({ tag: 'div' })).toBeTruthy()
+  expect(can.render({ tag: 'fragment' })).toBeTruthy()
+  expect(can.render({ tag: 'asd' })).toBeInstanceOf(window.Error)
+})
