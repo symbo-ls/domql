2aacd2d2c3ffff1dc91e978b3335f06478bbda14
nikoloza <nika.tomadze@gmail.com>
2021-12-04 04:47:07 +0400

    added domql as a package

---
+++ packages/react-transformer/index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+import React, { useEffect, useRef } from 'react'
+import { create } from '@domql/element'
+import { clone } from '@domql/utils'
+
+function DOMQLRenderer (component, parent, props) {
+  create({
+    proto: component,
+    props: clone(props)
+  }, parent)
+}
+
+export const reactTransformer = (component, props, state) => {
+  const ref = useRef(null)
+  return (
+    <React.Fragment ref={ref}>
+      {props.text}
+      {
+      useEffect(() => {
+        DOMQLRenderer(component, ref.current, props)
+      })
+    }
+    </React.Fragment>
+  )
+}
