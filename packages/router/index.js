ff06fb46e6e718d2744c7d432d599aa20f592021
nikoloza <nika.tomadze@gmail.com>
2022-09-18 18:59:12 +0400

    fixed router options

--- packages/router/index.js
+++ packages/router/index.js
@@ -1,36 +1,42 @@
 'use strict'
 
+import { merge } from '@domql/utils'
+
 export const getActiveRoute = (
   route = window.location.pathname, level
 ) => `/${route.split('/')[level + 1]}`
 
 export let lastLevel = 0
 
+const defaultOptions = {
+  level: lastLevel,
+  pushState: true,
+  scrollTo: true
+}
+
 export const router = (
   element,
   path,
   state = {},
-  options = {
-    level: lastLevel,
-    pushState: true
-  }
+  options = defaultOptions
 ) => {
-  const level = lastLevel = options.level
-  const pushState = options.pushState || options.pushState === undefined
+  merge(options, defaultOptions)
+  lastLevel = options.lastLevel
 
   const [pathname, hash] = (path).split('#')
 
-  const route = getActiveRoute(pathname, level)
+  const route = getActiveRoute(pathname, options.level)
   const content = element.routes[route]
 
   if (content) {
-    if (pushState) window.history.pushState(state, null, pathname + (hash ? `#${hash}` : ''))
+    if (options.pushState) window.history.pushState(state, null, pathname + (hash ? `#${hash}` : ''))
 
     element.set({ extend: content })
     element.state.update({ route, hash })
 
     const rootNode = element.node
-    rootNode.scrollTo({ behavior: 'smooth', top: 0, left: 0 })
+    if (options.scrollTo) rootNode.scrollTo({ behavior: 'smooth', top: 0, left: 0 })
+
     if (hash) {
       const activeNode = document.getElementById(hash)
       if (activeNode) {
