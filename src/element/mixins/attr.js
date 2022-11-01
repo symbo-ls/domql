a0ecdc0e490126c1bfa704175deb7cc47e3dfcfc
nikoloza <nika.tomadze@gmail.com>
2022-11-01 18:19:19 +0400

    removed attr caching

--- src/element/mixins/attr.js
+++ src/element/mixins/attr.js
@@ -8,15 +8,15 @@ import { exec, report } from '../../utils'
  */
 export default (params, element, node) => {
   const { __attr } = element
+  if (isNot('object')) report('HTMLInvalidAttr', params)
   if (params) {
-    if (isNot('object')) report('HTMLInvalidAttr', params)
     for (const attr in params) {
-      // if (!node) node = element.node
       const val = exec(params[attr], element)
-      if (__attr[attr] === val) return
+      // if (__attr[attr] === val) return
       if (val && node.setAttribute) node.setAttribute(attr, val)
       else if (node.removeAttribute) node.removeAttribute(attr)
       __attr[attr] = val
     }
   }
+  console.groupEnd(params, __attr)
 }
