8e7d121aaf1d70a020ddd33b9115ee4b03d0b355
nikoloza <nika.tomadze@gmail.com>
2023-01-22 13:30:44 +0400

    fixed globals export

--- packages/globals/index.js
+++ packages/globals/index.js
@@ -1,4 +1,7 @@
 'use strict'
 
-export const window = typeof window === 'undefined' ? global : window // eslint-disable-line
-export const document = typeof document === 'undefined' ? global.document : window.document // eslint-disable-line
+const window = typeof window === 'undefined' ? global : window // eslint-disable-line
+const document = typeof document === 'undefined' ? global.document : window.document // eslint-disable-line
+
+export { window, document }
+export default window
