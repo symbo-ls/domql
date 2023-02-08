fbb8c171dad085aaeaed957483f6216c8d02ff3d
nikoloza <nika.tomadze@gmail.com>
2023-02-09 02:27:35 +0400

    router export

--- packages/router/index.js
+++ packages/router/index.js
@@ -4,7 +4,7 @@ import { window } from '@domql/globals'
 
 export const splitRoute = (route = window.location.pathname) => route.slice(1).split('/')
 
-export default (rootElement, path, state = {}, level = 0, pushState = true) => {
+export const router = (rootElement, path, state = {}, level = 0, pushState = true) => {
   const route = path || window.location.pathname
   const routes = splitRoute(route)
 
@@ -18,3 +18,5 @@ export default (rootElement, path, state = {}, level = 0, pushState = true) => {
 
   if (level === 0) rootElement.state.update({ activePage: routes[level] })
 }
+
+export default router
