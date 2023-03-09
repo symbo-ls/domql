a9d857e1134c39974f943995434888428fffef3d
nikoloza <nika.tomadze@gmail.com>
2023-03-09 22:00:41 +0400

    added env package

---
+++ packages/env/index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const ENV = process.env.NODE_ENV
+
+export const isProduction = (ENV === 'production') || (ENV !== 'development' && ENV !== 'test')
+export const isTest = ENV === 'test'
+export const isDevelopment = ENV === 'development'
