c8d62c1454a5803d5cbba469451caea4b234bf0b
nikoloza <nika.tomadze@gmail.com>
2023-03-11 02:28:22 +0400

    export env

--- packages/env/index.js
+++ packages/env/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const ENV = process.env.NODE_ENV
+export const ENV = process.env.NODE_ENV
 
 export const isProduction = (ENV === 'production') || (ENV !== 'development' && ENV !== 'test')
 export const isTest = ENV === 'test'
