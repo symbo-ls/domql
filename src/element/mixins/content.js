2bd459b673af3ca163f01e1616fd9a5c807c7468
nikoloza <nika.tomadze@gmail.com>
2023-04-22 10:10:50 +0400

    added extra check on collection

--- src/element/mixins/content.js
+++ src/element/mixins/content.js
@@ -8,12 +8,14 @@ import set from '../set'
  */
 export default (param, element, node, options) => {
   if (param && element) {
+    const preventDefineUpdate = element.$setStateCollection ? '$setStateCollection' : '$setPropsCollection'
     if (element.$setCollection || element.$setStateCollection || element.$setPropsCollection) return
     if (param.__hash === element.content.__hash && element.content.update) {
       if (!element.content.__ref) element.content.__ref = {}
-      element.content.update(param)
+      element.content.update(param, { ...options, preventDefineUpdate })
     } else {
-      set.call(element, param, options)
+      console.log('or here')
+      set.call(element, param, { ...options, preventDefineUpdate })
     }
   }
 }
