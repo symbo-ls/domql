b0626d7c8d76ee2284dbb4dc52f82f20db27a778
nikoloza <nika.tomadze@gmail.com>
2023-04-25 01:47:02 +0400

    added isNode and isHTML tests

--- packages/utils/node.js
+++ packages/utils/node.js
@@ -14,10 +14,39 @@ export const isNode = (node) => {
   )
 }
 
+/** @url https://github.com/jimbrittain/isHTMLElement/tree/master */
 export const isHtmlElement = obj => {
-  return (
-    typeof window.HTMLElement === 'object'
-      ? obj instanceof window.HTMLElement // DOM2
-      : obj && typeof obj === 'object' && obj !== null && obj.nodeType === 1 && typeof obj.nodeName === 'string'
-  )
+  let hasHTMLElement = false
+  try {
+    if (window.HTMLElement !== undefined) { hasHTMLElement = true }
+  } catch (e1) {}
+  if (obj !== null && obj !== undefined) {
+    if (hasHTMLElement && typeof obj === 'object') {
+      return ((obj instanceof window.HTMLElement))
+    } else {
+      try {
+        if ('nodeType' in obj) {
+          return ((obj.nodeType === 1))
+        } else if ('tagName' in obj) {
+          return (obj.tagName !== null)
+        } else { return ('canHaveHTML' in obj) }
+      } catch (e2) {
+        try {
+          if (obj.nodeType) {
+            return ((obj.nodeType === 1))
+          } else if (obj.tagName) {
+            return (obj.tagName !== null)
+          } else { return !!(obj.canHaveHTML) }
+        } catch (e3) {}
+        return false
+      }
+    }
+  } else {
+    if ('IMDebugger' in window) {
+      (new window.IMDebugger()).pass('isHTMLElement must be supplied something.')
+    } else {
+      console.log('isHTMLElement must be supplied something.')
+    }
+    return false
+  }
 }
