ba04459fec8fd6223a164958a794d12d1accb2e9
nikoloza <nika.tomadze@gmail.com>
2023-04-25 05:06:52 +0400

    isHtml element and isNode

--- packages/utils/node.js
+++ packages/utils/node.js
@@ -2,51 +2,19 @@
 
 import { window } from '@domql/globals'
 
-export const isNode = (node) => {
-  const { Node } = window
+export const isNode = (obj) => {
   return (
-    typeof Node === 'function'
-      ? node instanceof Node
-      : node &&
-        typeof node === 'object' &&
-        typeof node.nodeType === 'number' &&
-        typeof node.tag === 'string'
+    typeof Node === 'object'
+      ? obj instanceof window.Node
+      : obj && typeof obj === 'object' && typeof obj.nodeType === 'number' && typeof obj.nodeName === 'string'
   )
 }
 
+// Returns true if it is a DOM element
 export const isHtmlElement = obj => {
-  /** @url https://github.com/jimbrittain/isHTMLElement */
-  let hasHTMLElement = false
-  try {
-    if (window.HTMLElement !== undefined) { hasHTMLElement = true }
-  } catch (e1) {}
-  if (obj !== null && obj !== undefined) {
-    if (hasHTMLElement && typeof obj === 'object') {
-      return ((obj instanceof window.HTMLElement))
-    } else {
-      try {
-        if ('nodeType' in obj) {
-          return ((obj.nodeType === 1))
-        } else if ('tagName' in obj) {
-          return (obj.tagName !== null)
-        } else { return ('canHaveHTML' in obj) }
-      } catch (e2) {
-        try {
-          if (obj.nodeType) {
-            return ((obj.nodeType === 1))
-          } else if (obj.tagName) {
-            return (obj.tagName !== null)
-          } else { return !!(obj.canHaveHTML) }
-        } catch (e3) {}
-        return false
-      }
-    }
-  } else {
-    if ('IMDebugger' in window) {
-      (new window.IMDebugger()).pass('isHTMLElement must be supplied something.')
-    } else {
-      console.log('isHTMLElement must be supplied something.')
-    }
-    return false
-  }
+  return (
+    typeof HTMLElement === 'object'
+      ? obj instanceof window.HTMLElement // DOM2
+      : obj && typeof obj === 'object' && obj !== null && obj.nodeType === 1 && typeof obj.nodeName === 'string'
+  )
 }
