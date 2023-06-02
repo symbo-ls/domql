7200f56a6d31bb14863d685057940091ec5d79fe
nikoloza <nika.tomadze@gmail.com>
2023-06-02 11:34:46 +0400

    router updates

--- packages/router/index.js
+++ packages/router/index.js
@@ -1,11 +1,12 @@
 'use strict'
 
 import { document, window } from '@domql/globals'
-import { merge } from '@domql/utils'
 
-export const getActiveRoute = (
-  route = window.location.pathname, level
-) => `/${route.split('/')[level + 1]}`
+export const getActiveRoute = (level = 0, route = window.location.pathname) => {
+  const routeArray = route.split('/')
+  const activeRoute = routeArray[level + 1]
+  if (activeRoute) return `/${activeRoute}`
+}
 
 export let lastPathname
 export let lastLevel = 0
@@ -18,7 +19,6 @@ const defaultOptions = {
   scrollToNode: false,
   scrollNode: document && document.documentElement,
   scrollBody: false,
-  scrollDocument: true,
   useFragment: false,
   updateState: true,
   scrollToOffset: 0,
@@ -29,48 +29,50 @@ export const router = (
   path,
   element,
   state = {},
-  options = defaultOptions
+  passedOptions = {}
 ) => {
-  merge(options, defaultOptions)
+  const options = { ...defaultOptions, ...passedOptions }
   lastLevel = options.lastLevel
 
   const [pathname, hash] = path.split('#')
 
   const rootNode = element.node
-  const route = getActiveRoute(pathname, options.level)
-  const content = element.routes[route] || element.routes['/*']
-  const scrollNode = options.scrollDocument ? document.documentElement : rootNode
+  const route = getActiveRoute(options.level, pathname)
+  const content = element.routes[route || '/'] || element.routes['/*']
+  const scrollNode = options.scrollToNode ? rootNode : options.scrollNode
   const hashChanged = hash && hash !== window.location.hash.slice(1)
   const pathChanged = pathname !== lastPathname
   lastPathname = pathname
 
-  if (content) {
-    if (options.pushState) window.history.pushState(state, null, route + (hash ? `#${hash}` : ''))
+  if (!content) return
+  if (options.pushState) {
+    window.history.pushState(state, null, pathname + (hash ? `#${hash}` : ''))
+  }
 
-    if (pathChanged || !hashChanged) {
-      element.set({ tag: options.useFragment && 'fragment', extend: content })
-      if (options.updateState) {
-        element.state.update({ route, hash }, {
-          preventContentUpdate: !options.stateContentUpdate
-        })
-      }
+  if (pathChanged || !hashChanged) {
+    if (options.updateState) {
+      element.state.update({ route, hash }, { preventContentUpdate: true })
     }
 
-    if (options.scrollToTop) {
-      scrollNode.scrollTo({
-        ...(options.scrollToOptions || {}), top: 0, left: 0
-      })
-    }
-    if (options.scrollToNode) {
-      content.content.node.scrollTo({
-        ...(options.scrollToOptions || {}), top: 0, left: 0
-      })
-    }
+    element.set({
+      tag: options.useFragment && 'fragment',
+      extend: content
+    })
+  }
+
+  if (options.scrollToTop) {
+    scrollNode.scrollTo({
+      ...(options.scrollToOptions || {}), top: 0, left: 0
+    })
+  }
+  if (options.scrollToNode) {
+    content.content.node.scrollTo({
+      ...(options.scrollToOptions || {}), top: 0, left: 0
+    })
   }
 
   if (hash) {
     const activeNode = document.getElementById(hash)
-    console.log(hash, activeNode)
     if (activeNode) {
       const top = activeNode.getBoundingClientRect().top + rootNode.scrollTop - options.scrollToOffset || 0
       scrollNode.scrollTo({
