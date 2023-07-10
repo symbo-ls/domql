ef7a6a8e20433551372683551031ed8645f3ebe7
nikoloza <nika.tomadze@gmail.com>
2023-07-10 19:02:58 +0400

    added replaceLiteralsWithObjectFields in utils

--- packages/utils/string.js
+++ packages/utils/string.js
@@ -3,8 +3,25 @@
 export const stringIncludesAny = (str, characters) => {
   for (const char of characters) {
     if (str.includes(char)) {
-      return true;
+      return true
     }
   }
-  return false;
+  return false
+}
+
+export const replaceLiteralsWithObjectFields = (str, state) => {
+  return str.replace(/\{\{\s*((?:\.\.\/)+)?([^}\s]+)\s*\}\}/g, (_, parentPath, variable) => {
+    if (parentPath) {
+      const parentLevels = parentPath.split('../').filter(Boolean).length
+      let parentState = state
+      for (let i = 0; i < parentLevels; i++) {
+        parentState = parentState.parent
+      }
+      const value = parentState[variable.trim()]
+      return value ? `${value}` : ''
+    } else {
+      const value = state[variable.trim()]
+      return value ? `${value}` : ''
+    }
+  })
 }
