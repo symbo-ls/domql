619f85cad5ce89e6d4d4071d9f92e0f4a95276a5
nikoloza <nika.tomadze@gmail.com>
2023-07-11 04:44:39 +0400

    fixed replacing

--- packages/utils/string.js
+++ packages/utils/string.js
@@ -12,16 +12,19 @@ export const stringIncludesAny = (str, characters) => {
 export const replaceLiteralsWithObjectFields = (str, state) => {
   return str.replace(/\{\{\s*((?:\.\.\/)+)?([^}\s]+)\s*\}\}/g, (_, parentPath, variable) => {
     if (parentPath) {
-      const parentLevels = parentPath.split('../').filter(Boolean).length
+      const parentLevels = parentPath.match(/\.\.\//g).length
       let parentState = state
       for (let i = 0; i < parentLevels; i++) {
         parentState = parentState.parent
+        if (!parentState) {
+          return '' // Return an empty string if the parent level doesn't exist
+        }
       }
       const value = parentState[variable.trim()]
-      return value ? `${value}` : ''
+      return value !== undefined ? `${value}` : ''
     } else {
       const value = state[variable.trim()]
-      return value ? `${value}` : ''
+      return value !== undefined ? `${value}` : ''
     }
   })
 }
