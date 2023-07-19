5827ff5f708d5bd0b9ef5fb53f02252e352c2d69
nikoloza <nika.tomadze@gmail.com>
2023-07-19 18:47:25 +0400

    brackets regex in str utils

--- packages/utils/string.js
+++ packages/utils/string.js
@@ -15,33 +15,19 @@ export const stringIncludesAny = (str, characters) => {
  * @param {object} state - The object containing the values to substitute.
  * @returns {string} The modified string with placeholders replaced by values from the object.
  */
-
-const tokenRegex = /\{\{\s*((?:\.\.\/)+)?([^}\s]+)\s*\}\}/g
-
+const brackRegex = /\{\{\s*((?:\.\.\/)+)?([^}\s]+)\s*\}\}/g
 export const replaceLiteralsWithObjectFields = (str, state) => {
-  if (!tokenRegex.test(str)) return str
-
-  return str.replace(tokenRegex, (_, parentPath, variable) => {
+  if (!str.includes('{{')) return str
+  return str.replace(brackRegex, (_, parentPath, variable) => {
     if (parentPath) {
       const parentLevels = parentPath.match(/\.\.\//g).length
       let parentState = state
-
-      // Cache parent states
-      const parentStateCache = {}
       for (let i = 0; i < parentLevels; i++) {
+        parentState = parentState.parent
         if (!parentState) {
           return '' // Return an empty string if the parent level doesn't exist
         }
-
-        const cachedState = parentStateCache[parentState]
-        if (cachedState) {
-          parentState = cachedState
-        } else {
-          parentState = parentState.parent
-          parentStateCache[parentState] = parentState
-        }
       }
-
       const value = parentState[variable.trim()]
       return value !== undefined ? `${value}` : ''
     } else {
