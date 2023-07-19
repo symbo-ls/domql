deb120518950b9e0c8ee492b49cb5f3954a55c25
nikoloza <nika.tomadze@gmail.com>
2023-07-19 08:10:12 +0400

    cleanup performance

--- packages/utils/globals/index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const global = globalThis
-const self = globalThis
-const window = globalThis
-const document = window.document // eslint-disable-line
-
-export { global, self, window, document }
