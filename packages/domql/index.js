e74c5c215c97b92c1c61f0193ced01e59a795dc3
nikoloza <nika.tomadze@gmail.com>
2023-07-19 15:39:44 +0400

    return fixed

--- packages/domql/index.js
+++ packages/domql/index.js
@@ -1,11 +1,9 @@
 'use strict'
 
 import { TREE, create as createElement } from '@domql/element'
-import { createNode } from '@domql/render'
 
 const create = (element, parent, key, options) => {
-  createElement(element, parent, key, options)
-  createNode(element, options)
+  return createElement(element, parent, key, options)
 }
 
 export default {
