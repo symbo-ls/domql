4738caa80690c2fcf3d92f06fb902b5bc3075de4
nikoloza <nika.tomadze@gmail.com>
2023-10-21 08:41:40 +0400

    fixed state pages

--- packages/state/updateState.js
+++ packages/state/updateState.js
@@ -80,7 +80,8 @@ const hoistStateUpdate = (state, obj, options) => {
   const findGrandParentState = getParentStateInKey(stateKey, parent.state)
   const changesValue = createChangesByKey(stateKey, passedValue)
   const targetParent = findGrandParentState || parent.state
-  if (options.replace) targetParent[stateKey] = value
+  if (options.replace) overwriteDeep(targetParent, changesValue) // check with createChangesByKey
+  console.log(changesValue)
   targetParent.update(changesValue, {
     execStateFunction: false,
     isHoisted: true,
