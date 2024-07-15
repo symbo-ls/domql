ab2f612324ad23312ae69941182e7f29460548d0
nikoloza <nika.tomadze@gmail.com>
2024-07-15 07:33:15 +0400

    add new utils

--- packages/utils/array.js
+++ packages/utils/array.js
@@ -130,3 +130,16 @@ export const reorderArrayByValues = (array, valueToMove, insertBeforeValue) => {
   }
   return newArray
 }
+export const arraysEqual = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+
+  return true
+}
--- packages/utils/string.js
+++ packages/utils/string.js
@@ -54,3 +54,73 @@ export const replaceLiteralsWithObjectFields = (str, state, options = {}) => {
 export const lowercaseFirstLetter = (inputString) => {
   return `${inputString.charAt(0).toLowerCase()}${inputString.slice(1)}`
 }
+
+export const findKeyPosition = (str, key) => {
+  const lines = str.split('\n')
+  let startLineNumber = -1
+  let endLineNumber = -1
+  let startColumn = -1
+  let endColumn = -1
+
+  const keyPattern = new RegExp(`\\b${key}\\b\\s*:\\s*`)
+  let braceCount = 0
+  let foundKey = false
+
+  for (let i = 0; i < lines.length; i++) {
+    if (keyPattern.test(lines[i]) && !foundKey) {
+      foundKey = true
+      startLineNumber = i + 1
+      startColumn = lines[i].indexOf(key) + 1
+
+      // Check if the value is an empty object
+      if (lines[i].includes('{}')) {
+        endLineNumber = startLineNumber
+        endColumn = lines[i].indexOf('{}') + 3
+        break
+      }
+
+      // Check if the value starts with '{' (object) or '[' (array)
+      const line = lines[i].slice(startColumn + key.length)
+      if (line.includes('{') || line.includes('[')) {
+        braceCount = 1
+      } else {
+        endLineNumber = i + 1
+        endColumn = lines[i].length + 1
+        break
+      }
+    } else if (foundKey) {
+      braceCount += (lines[i].match(/{/g) || []).length
+      braceCount += (lines[i].match(/\[/g) || []).length
+      braceCount -= (lines[i].match(/}/g) || []).length
+      braceCount -= (lines[i].match(/]/g) || []).length
+
+      // If braceCount is 0 and we find the end of the object/array
+      if (braceCount === 0) {
+        endLineNumber = i + 1
+        endColumn = lines[i].lastIndexOf('}') !== -1 ? lines[i].lastIndexOf('}') + 2 : lines[i].length + 1
+        break
+      }
+    }
+  }
+
+  return {
+    startColumn,
+    endColumn,
+    startLineNumber,
+    endLineNumber
+  }
+}
+
+export const replaceOctalEscapeSequences = (str) => {
+  // Regex to match octal escape sequences
+  const octalRegex = /\\([0-7]{1,3})/g
+
+  // Replace each match with the corresponding character
+  return str.replace(octalRegex, (match, p1) => {
+    // Convert the octal value to a decimal integer
+    const octalValue = parseInt(p1, 8)
+    // Convert the decimal value to the corresponding character
+    const char = String.fromCharCode(octalValue)
+    return char
+  })
+}
