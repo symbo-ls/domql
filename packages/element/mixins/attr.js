4277d8291631b6cc49c5b7ba6293cc235f5fb217
Thomas <tzhang920314@gmail.com>
2025-09-09 19:23:49 -0400

    fix: prevents unnecessary DOM writes and stops <img src> from being re-set to the same URL

--- packages/element/mixins/attr.js
+++ packages/element/mixins/attr.js
@@ -15,7 +15,9 @@ export function attr(params, element, node) {
     if (props.attr) deepMerge(params, props.attr)
     for (const attr in params) {
       const val = exec(params[attr], element)
-      // if (__attr[attr] === val) return
+      // Skip setting the same attribute value to avoid unnecessary DOM writes
+      // and side-effects like <img> refetching the same src
+      if (__attr[attr] === val) continue
       if (
         val !== false &&
         !isUndefined(val) &&
